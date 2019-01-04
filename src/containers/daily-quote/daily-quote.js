6982de61410afcbc4d4d82f741fc766fe03f3e59
TangMichael <tangmichael6@hotmail.com>
2019-01-04 00:22:58 -0500

    scroll view, onEndReached load more data

--- src/containers/daily-quote/daily-quote.js
+++ src/containers/daily-quote/daily-quote.js
@@ -1,43 +1,78 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, View, Text, Button } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  ListView
+} from "react-native";
 import { connect } from "react-redux";
 import { addFavorites } from "../../actions/index";
 import { openDatabase } from "react-native-sqlite-storage";
-let db = openDatabase({ name: "db", createFromLocation: '~quotes.db'});
+import { ScrollView, FlatList } from "react-native-gesture-handler";
+let db = openDatabase({ name: "db", createFromLocation: "~quotes.db" });
 
 class DailyQuote extends Component {
+  textInputComponents;
   constructor(props) {
     super(props);
-    this.state = { quotes: [] };
+    this.state = { quotes: [], id: 0 };
   }
-
   componentDidMount() {
     db.transaction(tx => {
       // somehow needs an argument, without it, it doesnt work
       // but the argument should be optional
-      tx.executeSql("SELECT * FROM quotes",[], (tx, results) => {
+      tx.executeSql("SELECT * FROM quotes where id < 11", [], (tx, results) => {
         var len = results.rows.length;
-        var arr = []
+        var arr = [];
         if (len > 0) {
-          for(var i = 0; i<10; i++){
+          for (var i = 0; i < 10; i++) {
             arr.push(results.rows.item(i).quote);
           }
-          this.setState({quotes: arr})
-          console.log(arr)
+          this.setState({ quotes: arr, id: 10 });
         }
       });
     });
   }
 
+  addQuotes() {
+    db.transaction(tx => {
+      // somehow needs an argument, without it, it doesnt work
+      // but the argument should be optional
+      tx.executeSql(
+        "SELECT * FROM quotes where id <" + (this.state.id + 11) + " AND id >"+ this.state.id,
+        [],
+        (tx, results) => {
+          var len = results.rows.length;
+          var arr = [];
+          if (len > 0) {
+            for (var i = 0; i < 10; i++) {
+              arr.push(results.rows.item(i).quote);
+            }
+            this.setState({
+              quotes: this.state.quotes.concat(arr),
+              id: this.state.id + 10
+            });
+          console.log(this.state.quotes);
+          }
+        }
+      );
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>{this.state.quotes}</Text>
-        <Button
-          title="Add to favorites"
-          onPress={() => this.props.addFavorites("ok")}
-        />
-      </View>
+      <FlatList
+        data={this.state.quotes}
+        renderItem={({ item }) => (
+          <Text style={{ borderBottomWidth: 1 }}>{item}</Text>
+        )}
+        onEndReachedThreshold={0.01}
+        onEndReached={() => {
+          this.addQuotes(() => console.log(this.state.id));
+        }}
+      />
     );
   }
 }
